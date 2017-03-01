60e096f8738d99018a85ef207e919bd93dde4bbd
Michael Chong <Michael.Chong@optiver.com.au>
2017-03-02 10:20:22 +1100

    Implement round to nearest. TODO: roll the adjust rounding bits into the normalization to clean up the ugly.

--- src/FloatAddSingle.hs
+++ src/FloatAddSingle.hs
@@ -26,17 +26,24 @@ fadds a b = result
 
     leadingOneIdx = leadingOneDetector mantRes
 
-    mantRes' :: Unsigned 24
-    mantRes'
-        | overflow  = unpack $ (1 :: Bit) ++# (slice d23 d1 mantRes)
-        | otherwise = normalizationShift mantRes g leadingOneIdx 
+--    mantRes' :: Unsigned 24
+    (mantRes', g', r', s') = normalizationShift mantRes leadingOneIdx overflow g r s
+--        | overflow  = unpack $ (1 :: Bit) ++# (slice d23 d1 mantRes)
+--        | otherwise = normalizationShift mantRes g leadingOneIdx 
 
     expRes :: Unsigned 8
     expRes
         | overflow  = expA + 1
         | otherwise = normalizeExponent expA leadingOneIdx
 
-    result = (0 :: Bit) ++# pack expRes ++# (slice d22 d0 mantRes')
+    (r'', s'') = adjustRoundingBits (not overflow && leadingOneIdx > 0) g' r' s'
+
+    (mantRounded, roundOverflow) = roundMantissa mantRes' r'' s''
+
+    expRounded = bool expRes (expRes + 1) roundOverflow
+
+    result = (0 :: Bit) ++# pack expRounded ++# (slice d22 d0 mantRounded)
+
 
 -- Unpack single precision float into sign, exponent and mantissa.
 -- Implied leading 1 is appended to mantissa
@@ -93,14 +100,45 @@ leadingOneDetector a = fromIntegral $ countLeadingZeros a
 
 -- If there are leading zeroes, shift mantissa left until no more leading zeroes.
 -- Shift in the guard bit.
-normalizationShift :: Unsigned 24 -> Bit -> Index 24 -> Unsigned 24
-normalizationShift mant guard shiftAmount = result
+normalizationShift :: Unsigned 24 -> Index 24 -> Bool -> Bit -> Bit -> Bit -> (Unsigned 24, Bit, Bit, Bit)
+normalizationShift mant shiftAmount overflowed g r s = (result, g', r', s')
     where
-    temp = pack mant ++# guard
-    shifted = temp `shiftL` fromIntegral shiftAmount
+    temp = pack mant ++# g
+    shifted
+        | overflowed = (1 :: Bit) ++# slice d24 d1 temp
+        | otherwise  = temp `shiftL` fromIntegral shiftAmount
     result = unpack $ slice d24 d1 shifted
+    g' = lsb shifted
+    r'
+        | overflowed = g .|. r
+        | otherwise  = r
+    s'
+        | overflowed = r .|. s
+        | otherwise  = s
 
 normalizeExponent :: Unsigned 8 -> Index 24 -> Unsigned 8
 normalizeExponent exp shiftAmount = exp - (fromIntegral shiftAmount)
 
+adjustRoundingBits :: Bool -> Bit -> Bit -> Bit -> (Bit, Bit)
+adjustRoundingBits normShift g r s = (r', s')
+    where
+    r'
+        | normShift = r
+        | otherwise = g
+    s'
+        | normShift = s
+        | otherwise = r .|. s
 
+-- round to nearest
+roundMantissa :: Unsigned 24 -> Bit -> Bit -> (Unsigned 24, Bool)
+roundMantissa a r s = (result, overflow)
+    where
+    plus1 = (r .&. lsb a) .|. (r .&. s) == 1
+    temp :: Unsigned 25
+    temp
+        | plus1     = (resize a :: Unsigned 25) + 1
+        | otherwise = resize a :: Unsigned 25
+    overflow = (msb temp == 1)
+    result
+        | overflow  = unpack $ slice d24 d1 temp
+        | otherwise = unpack $ slice d23 d0 temp
